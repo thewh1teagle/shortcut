9346d56ac133dc20d2947e3af16b4f2d2dfff5a1
Yehoyada <hvuhsg6@gmail.com>
2024-04-03 23:49:14 +0300

    second

--- main.go
+++ main.go
@@ -1,56 +1,83 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"os/exec"
+	"strings"
 
-	// "github.com/go-vgo/robotgo"
 	hook "github.com/robotn/gohook"
+	"github.com/xeipuuv/gojsonschema"
 )
 
-func main() {
-	add()
-	low()
-	event()
+const configPath = "shortcuts.json"
+const jsonSchemaPath = "schema.json"
+
+type Shortcut struct {
+	Name      string
+	Shortcuts []string
+	Action    string
 }
 
-func add() {
-	fmt.Println("--- Please press ctrl + shift + q to stop hook ---")
-	hook.Register(hook.KeyDown, []string{"q", "ctrl", "shift"}, func(e hook.Event) {
-		fmt.Println("ctrl-shift-q")
-		hook.End()
-	})
+func main() {
 
-	fmt.Println("--- Please press w---")
-	hook.Register(hook.KeyDown, []string{"w"}, func(e hook.Event) {
-		fmt.Println("w")
-	})
+	shortcuts, err := parseShrotcuts()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(shortcuts)
+	registerShortcuts(shortcuts)
 
 	s := hook.Start()
 	<-hook.Process(s)
 }
 
-func low() {
-	evChan := hook.Start()
-	defer hook.End()
-
-	for ev := range evChan {
-		fmt.Println("hook: ", ev)
+func registerShortcuts(shortcuts []Shortcut) {
+	for _, shortcut := range shortcuts {
+		hook.Register(hook.KeyDown, shortcut.Shortcuts, func(e hook.Event) {
+			fmt.Printf("Shortcut <%s> activated", shortcut.Name)
+			command := strings.Split(shortcut.Action, " ")
+			if len(command) == 1 {
+				exec.Command(command[0])
+			} else {
+				exec.Command(command[0], command[1:]...)
+			}
+		})
 	}
 }
 
-func event() {
-	ok := hook.AddEvents("q", "ctrl", "shift")
-	if ok {
-		fmt.Println("add events...")
+func parseShrotcuts() ([]Shortcut, error) {
+	// Load the JSON schema
+	schemaLoader := gojsonschema.NewStringLoader(jsonSchemaPath)
+	schema, err := gojsonschema.NewSchema(schemaLoader)
+	if err != nil {
+		return nil, err
 	}
 
-	keve := hook.AddEvent("k")
-	if keve {
-		fmt.Println("you press... ", "k")
+	// Load the JSON data
+	documentLoader := gojsonschema.NewStringLoader(configPath)
+
+	// Validate the JSON data against the schema
+	result, err := schema.Validate(documentLoader)
+	if err != nil {
+		return nil, err
 	}
 
-	mleft := hook.AddEvent("mleft")
-	if mleft {
-		fmt.Println("you press... ", "mouse left button")
+	if !result.Valid() {
+		fmt.Printf("The JSON data is invalid: %s\n", result.Errors())
+		return nil, fmt.Errorf("invalid JSON data")
 	}
+
+	// Unmarshal the validated JSON data
+	var data struct {
+		Version   string     `json:"version"`
+		Shortcuts []Shortcut `json:"shortcuts"`
+	}
+	err = json.Unmarshal([]byte(jsonStr), &data)
+	if err != nil {
+		return nil, err
+	}
+
+	return data.Shortcuts, nil
 }
